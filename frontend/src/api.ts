0d2ee01cb5ab3281c89162e19228f0c1c3a1c0a6
Alex <alexandr.gradov@mail.ru>
2025-07-05 12:02:06 +0500

    cors

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: 'http://localhost:8000',
 })
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get['Accepts'] = 'application/json'
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8'
+
 export default api
